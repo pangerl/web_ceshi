21f886e318962de28632a89868da994adce949ac
薄荷盐 <hxjagf@qq.com>
2019-01-05 14:18:23 +0800

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-username = 'cqteacher1'
-password = 'cqteacher'
-
-smtp_host = 'smtp.exmail.qq.com'
-mail_user = 'notify@ahacode.com'
-mail_pwd = 'Ahalei@2018'
-mail_to_list = ['huangxiaojun2135@codeaha.com', 'hxjagf@qq.com']
-mail_title = '自动化测试告警邮件'
